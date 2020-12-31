1d6392b9fb0dc4165ea7ad8174dc22dcdce19f72
djgingersteve <42731097+djgingersteve@users.noreply.github.com>
2020-12-31 15:57:20 +0000

    Update start.sh
    Amended to fix linear volume issue

--- plugins/spotify/start.sh
+++ plugins/spotify/start.sh
@@ -36,7 +36,7 @@ set -- /usr/bin/librespot \
   --name "$SOUND_DEVICE_NAME" \
   --bitrate "$SOUND_SPOTIFY_BITRATE" \
   --cache /var/cache/raspotify \
-  --linear-volume \
+  --mixer-linear-volume \
   "$@"
 
 exec "$@"
